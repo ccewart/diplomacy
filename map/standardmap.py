00a0c947d1a4ab11a821b751ebf07757d0b2bed9
GoandgowithMilo <jamesmilo42@gmail.com>
2019-07-27 13:35:49 +1000

    updated territority and map, as well as added standardmap for base implementation

---
+++ map/standardmap.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 13:31:00 2019
+
+@author: Jamesrulz
+"""
+
+import map
+
+### this file will introduce all the territories as per a standard game of diplomacy
--- map/territory.py
+++ map/territory.py
@@ -5,60 +5,54 @@ Created on Sat Jul 13 06:47:02 2019
 @author: Jamesrulz
 """
 
-''' territories should map out the relationships between the three types in a hierchical fashion '''
-'''
- This can be implemented through the following rule set adjacencies: list of tuples of names representing adjacent territories. Must be legal:
-        * Sea adjacent only to Sea or Coast
-        * Coast adjacent only to Sea or Coast
-        * Land adjacent only to Land
-        * All names must be provided in territory_descriptors
-        * No duplicate adjacencies!
-'''
+class subtypeException(Exception):
+    pass
+
+class supplyCentreException(Exception):
+    pass
 
 class Territory:
     ''' defines a territory which is the unit that the map consists of '''
-    ''' a territory has a name and a set of adjacent territories '''
-    name = None
-    adjacencies = None
+    ''' a territory has a name, a set of adjacent territories, a subtype and a flag for supply centre '''
+    name = None # name of the territory
+    adjacent = None # name of territories adjacent
+    subtype = None # land, coast or sea
+    supplyCentre = None # boolean flag indicating whether territory is a supply centre or not
+    owner = None # the name of the player who owns the territory
     
-    def __init__(self, name, adjacencies): # name and adjacency restrictions implemented in map.py
-        self.name = name # should probably enforce name restrictions (i.e. only ww1 territory names)
-        self.adjacencies = []
-        for i in adjacencies:
-            self.adjacencies.append(i) # same point as above but for adjacency
+    def __init__(self, name, adjacent, subtype, supplyCentre, owner):
+        self.name = name
         
+        self.adjacent = [] # need to enforce that these are only from the list of given territories
+        for territories in adjacent:
+            self.adjacent.append(territories)
+        
+        if subtype not in ['land', 'sea', 'coast']: # this doesn't handle if someone inputs multiple or duplicate subtypes
+            raise subtypeException('a territory must be of the type land, sea or coast')
+        else:
+            self.subtype = subtype
+            
+        if not isinstance(supplyCentre, bool):
+            raise supplyCentreException('a territory can only be a supply centre or not be a supply centre')
+        else:
+            self.supplyCentre = supplyCentre
+            
+        self.owner = owner # not certain whether this should be owner or whether a territory contains a unit, or both?
+    
     def __str__(self):
         return self.name
     
     def get_name(self):
         return self.name
     
-    def get_adjacencies(self):
-        return self.adjacencies
+    def get_adjacent(self):
+        return self.adjacent
     
-    def no_duplicate_territories(self, other):
-        try:
-            assert self.name != other
-        except AssertionError:
-            print("This territory already exists on the map")
-
-territoryList = ['Austria-Hungary', 'England', 'France', 'Germany', 'Italy', 'Russia', 'Turkey']
-
-worldMap = list()
-
-for i in range(len(territoryList)):
-    worldMap.append(Territory(territoryList[i], ['none']))
+    def get_subtype(self):
+        return self.subtype
     
+    def get_supplyCentre(self):
+        return self.supplyCentre
     
-        
-# TODO
-# want to implement these when unit types are defined. They should be enforcing movement of certain unit types only
-
-#class LandTerritory(Territory):
-#    '''specific features of a territory that is landlocked'''
-#        
-#class CoastTerritory(Territory):
-#    '''specific features of a land territory that is on the coast'''
-#    
-#class SeaTerritory(Territory):
-#    '''features of a seabased territory'''
+    def get_owner(self):
+        return self.owner
